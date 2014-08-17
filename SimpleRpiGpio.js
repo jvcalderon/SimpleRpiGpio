82ad89ffe2c2837bed078c510e4f945fac70c5f9
José Vte. Calderón <jvcprod@hotmail.com>
2014-08-17 12:50:27 +0200

    New methods added

--- SimpleRpiGpio.js
+++ SimpleRpiGpio.js
@@ -34,27 +34,81 @@ SimpleRpiGpio.prototype.__construct = function () {
 };
 
 /**
- * Send signal to Rpi's GPIO pin number
+ * open method open a GPIO pin. This action is needed before read or write
+ *
+ * @param pinNumber
+ * @param direction in or out
+ */
+SimpleRpiGpio.prototype.open = function (pinNumber, direction) {
+
+    var gpioNumber = this.getGpioNumber(pinNumber);
+
+    if (gpioNumber !== false &&
+        (direction == 'in' || direction == 'out')) {
+        this.exec(
+            'echo ' + gpioNumber + ' > /sys/class/gpio/unexport &' +
+            'echo ' + gpioNumber + ' > /sys/class/gpio/export &' +
+            'echo ' + direction + ' > /sys/class/gpio/gpio' + gpioNumber + '/direction'
+        );
+    }
+    else {
+        console.log('Only allowed "in" or "out" values for direction');
+    }
+};
+
+/**
+ * openAll method open all GPIO pins with same direction
+ *
+ * @param direction in or out
+ */
+SimpleRpiGpio.prototype.openAll = function(direction) {
+    for(pinNumber in this.pinMapping) {
+        if(this.pinMapping.hasOwnProperty(pinNumber)) {
+            this.open(this.pinMapping[pinNumber], direction);
+        }
+    }
+};
+
+/**
+ * out method sends signal to Rpi's GPIO pin number
  *
  * @param pinNumber
  * @param action bool (0 = LV | 1 = HV)
  */
 SimpleRpiGpio.prototype.out = function (pinNumber, action) {
+    var gpioNumber = this.getGpioNumber(pinNumber);
+    if (gpioNumber !== false) {
+        this.exec('echo ' + action + ' > /sys/class/gpio/gpio' + gpioNumber + '/value');
+    }
+};
 
+/**
+ * exec method executes a UNIX command
+ *
+ * @param command
+ */
+SimpleRpiGpio.prototype.exec = function (command) {
+    child = exec(command,
+        function (error) {
+            if (error !== null) {
+                console.log('exec error: ' + error);
+            }
+        });
+};
+
+/**
+ * Get the GPIO number associated to pin number
+ *
+ * @param pinNumber
+ * @returns false when pinNumber is not valid
+ */
+SimpleRpiGpio.prototype.getGpioNumber = function (pinNumber) {
     var gpioNumber = this.pinMapping[pinNumber];
-
     if (gpioNumber != undefined) {
-        child = exec('echo ' + action + ' > /sys/class/gpio/gpio' + gpioNumber + '/value',
-            function (error) {
-                if (error !== null) {
-                    console.log('exec error: ' + error);
-                }
-            });
+        return gpioNumber;
     }
-    else {
-        console.log(pinNumber + ' is not a valid pin number');
-    }
-
+    console.log(pinNumber + ' is not a valid pin number');
+    return false;
 };
 
 module.exports = SimpleRpiGpio;
